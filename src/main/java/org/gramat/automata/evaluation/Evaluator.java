33db67da6fdc874d037db7a3374a54665afa2773
sergiouph <sergio.uriel.ph@gmail.com>
2021-06-18 13:51:12 -0500

    Add debug logs

--- src/main/java/org/gramat/automata/evaluation/Evaluator.java
+++ src/main/java/org/gramat/automata/evaluation/Evaluator.java
@@ -1,8 +1,11 @@
 package org.gramat.automata.evaluation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.gramat.automata.State;
 import org.gramat.automata.tapes.Tape;
+import org.gramat.tools.PP;
 
+@Slf4j
 public class Evaluator {
 
     public Object eval(State initial, Tape tape) {
@@ -14,15 +17,27 @@ public class Evaluator {
         var state = initial;
 
         while (tape.isOpen()) {
-            var symbol = tape.getChar();
-            var transition = state.findTransitionFor(symbol, context.getToken());
+            var ch = tape.getChar();
+            var transition = state.findTransitionFor(ch, context.getToken());
             if (transition == null) {
                 break;
             }
 
+            log.debug("MOVE TO {} @ {}", PP.ch(ch), tape.getLocation());
+
+            for (var action : transition.getBeginActions()) {
+                log.debug("RUN BEGIN {}", action);
+            }
+
             context.run(transition.getBeginActions());
 
+            log.debug("MOVE FORWARD");
+
             tape.moveForward();
+
+            for (var action : transition.getEndActions()) {
+                log.debug("RUN END {}", action);
+            }
 
             context.run(transition.getEndActions());
 
